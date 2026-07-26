eac76cb582afcca6821daf1e63afc9c2c8aa0249
Axeniter <anamievoid@gmail.com>
2026-07-26 13:17:09 +1000

    Feat: activity model

---
+++ aura_flow/lib/models/activity.dart
@@ -0,0 +1,45 @@
+import 'package:hive/hive.dart';
+import 'package:uuid/uuid.dart';
+
+part 'activity.g.dart';
+
+@HiveType(typeId: 2)
+class Activity extends HiveObject {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  String name;
+
+  @HiveField(2)
+  String? description;
+
+  @HiveField(3)
+  String icon;
+
+  @HiveField(4)
+  String skillId;
+
+  Activity({
+    required this.id,
+    required this.name,
+    this.description,
+    required this.icon,
+    required this.skillId,
+  });
+
+  factory Activity.create({
+    required String name,
+    String? description,
+    required String icon,
+    required String skillId,
+  }) {
+    return Activity(
+      id: Uuid().v1(),
+      name: name, 
+      description: description,
+      icon: icon, 
+      skillId: skillId
+    );
+  }
+}
